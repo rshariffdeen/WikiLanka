f74a35b5cdb99cb004cd50ea8c5bdeca47d2f218
ridwannaruto <ridwan.11@cse.mrt.ac.lk>
2014-01-05 12:57:48 +0530

    Initialize Symfony 2.40

---
+++ app/cache/prod/twig/e3/bc/64fa16221dd558d24434ad3a10e1251bd74912b5f513489a414c34d9165f.php
@@ -0,0 +1,39 @@
+<?php
+
+/* WikiLankaStyleBundle:Default:index.html.twig */
+class __TwigTemplate_e3bc64fa16221dd558d24434ad3a10e1251bd74912b5f513489a414c34d9165f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : null), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "WikiLankaStyleBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/WikiLanka/Bundle/StyleBundle/WikiLankaStyleBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WikiLanka\Bundle\StyleBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WikiLankaStyleBundle extends Bundle
+{
+}
