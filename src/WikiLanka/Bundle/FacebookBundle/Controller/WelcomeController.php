0cb5b5c412f4e7984e084d007b35397532f3f082
rajeerc <rajeerc@gmail.com>
2014-01-31 17:40:03 +0530

    Welcome page added

--- src/WikiLanka/Bundle/FacebookBundle/Controller/WelcomeController.php
+++ src/WikiLanka/Bundle/FacebookBundle/Controller/WelcomeController.php
@@ -2,12 +2,41 @@
 
 namespace WikiLanka\Bundle\FacebookBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class WelcomeController extends Controller
-{
-    public function startAction($name)
-    {
-        return $this->render('WikiLankaFacebookBundle:Welcome:index.html.twig', array('name' => $name));
+class WelcomeController extends Controller {
+
+    public function startAction(Request $request) {
+        $number = $request->get('number');
+        $token = $request->get('token');
+        $user = $request->get('user');
+
+        require 'facebook.php';
+        $facebook = new Facebook(array(
+                    'appId' => '453545681427596',
+                    'secret' => '9885617e068739cb01b952b2ab571c01',
+                ));
+
+
+        if ($user) {
+            $query = mysql_query("SELECT PIN FROM `wikilanka`.`database`");
+            $pin = 1;
+            $array = array();
+            $counter = 1;
+            while ($row = mysql_fetch_array($query)) {
+
+                $array[$counter] = $row['PIN'];
+                //echo $array[$counter];
+                $counter++;
+            }
+            while (array_search($pin, $array)) {
+                $pin = rand(1000, 9999);
+            };
+            //echo $pin;
+            mysql_query("UPDATE `wikilanka`.`database` SET `pin`='{$pin}' WHERE `mobile`='{$number}'", $dbhandle);
+            return $this->render('WikiLankaFacebookBundle:Welcome:welcome.html.twig', array('user' => $user, 'pin' => $pin));
+        }
     }
+
 }
